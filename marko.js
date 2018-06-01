8b868b678203f4f702ca4ebcda0da0895978853b
l.gil <l.gil@AR-IT10026>
2018-06-01 05:34:21 -0300

    test 2

--- marko.js
+++ marko.js
@@ -8,6 +8,8 @@ module.exports = function(RED) {
 		this.name = n.name;
 		this.field = n.field || "payload";
 
+	
+
 		if (RED.storage && RED.storage.projects) {
 			var activeProject = RED.storage.projects.getActiveProject();
 
@@ -17,17 +19,20 @@ module.exports = function(RED) {
 			}
 		}
 
+		this.path = n.path || "";
+		this.projectName = n.projectName || "";
+
 		this.templatesFolder = n.templatesFolder || "templates";
 		this.template = n.template || "";
 
 		var node = this;
 		node.on('input', function(msg) {
 			var temFile = path.join(node.path,node.template);
-			console.log(temFile);
+			RED.log.info(temFile);
 			var tepl = require(temFile);
-			var value = tepl.renderToString(msg);
+			var value = tepl.renderToString(msg) || "";
 
-			console.log(value);
+			RED.log.info(value);
 			RED.util.setMessageProperty(msg,node.field,value);
 			node.send(msg);
 		});
